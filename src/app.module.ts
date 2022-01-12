3c09dee2e3f25f5f76ada3a42026f8229c509c64
GabbsCoding <gaby.ballestger@gmail.com>
2022-01-13 00:52:02 +0100

    Apply global config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Key } from './common/enum';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number;
+  static host: string;
+  static version: string;
+
+  constructor(private readonly configService: ConfigService) {
+    AppModule.port = +this.configService.get(Key.SERVER_PORT);
+    AppModule.host = this.configService.get(Key.SERVER_HOST);
+    AppModule.version = this.configService.get(Key.SERVER_VERSION);
+  }
+ 
+  
+}
