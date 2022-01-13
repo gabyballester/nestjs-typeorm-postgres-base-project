448c27a426e4ebd0f302a91e0901f3604f297b97
GabbsCoding <gaby.ballestger@gmail.com>
2022-01-13 01:00:40 +0100

    Server config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { port, host, version } = AppModule;
+  const globalPrefix = `/api/${version}`;
+
+  app.setGlobalPrefix(globalPrefix)    
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }));
+  app.use(helmet());
+  app.use(compression());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    }),
+  );
+  
+  await app.listen(port);
+  const serverUrl = `Server listening on http://${host}:${port+globalPrefix}`;
+  console.log(`Server listening on ${serverUrl}`);  
+  app.use(csurf());
 }
 bootstrap();
